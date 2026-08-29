41ee634bc80bc2938be85773f7f126259f50c756
Szabolcs <szabolcs.pall@aol.com>
2026-08-29 12:00:26 +0200

    Update

--- app/src/main/java/com/mangocodex/HighlightingEditText.kt
+++ app/src/main/java/com/mangocodex/HighlightingEditText.kt
@@ -138,6 +138,8 @@ class HighlightingEditText(context: Context) : AppCompatEditText(context) {
         val start = range.first.coerceIn(0, length)
         val end = (range.last + 1).coerceIn(0, length)
         requestFocus()
-        setSelection(start, end)
+        post {
+            setSelection(start, end)
+        }
     }
 }
